d54769868dcf5af62c082307c5cd3309bd60b349
alena_alekhina <pugofka08@mail.ru>
2022-01-24 12:35:45 +0500

    exercise2

---
+++ data.js
@@ -0,0 +1,8 @@
+export const string = 'Hello1!';
+
+
+const data = {
+  alert: 'Hello2!',
+};
+
+export default data;
---
+++ main.js
@@ -0,0 +1,5 @@
+import { showAlert } from './function.js';
+import data, { string } from './data.js';
+
+showAlert(string);
+showAlert(data.alert);
